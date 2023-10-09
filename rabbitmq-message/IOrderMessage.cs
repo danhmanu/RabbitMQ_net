e72d9b1ea37943f3d516aebed018406ccae642e6
ADMIN\Admin <danhmanu@gmail.com>
2023-10-09 19:01:19 +0700

    - Core Bus RabbitMQ

---
+++ rabbitmq-message/IOrderMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabbitmq_message
+{
+    public class IOrderMessage
+    {
+        public Guid OrderId { get; set; }
+        public string PaymentCardNumber { get; set; }
+        public string ProductName { get; set; }
+    }
+}
---
+++ rabbitmq/rabbitmq/BusConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabbitmq
+{
+    public class BusConstants
+    {
+        public const string RabbitMqUri = "rabbitmq://localhost:5673/";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string OrderQueue = "validate-order-queue";
+    }
+}
